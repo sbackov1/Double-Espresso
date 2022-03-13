895b6a3689f63d95b018232e89d48115be26f1c3
Jared Allen <jla4782@truman.edu>
2022-03-12 20:42:34 -0500

    Adding clueless-server skeleton

---
+++ clueless-client/src/test/java/edu/jhu/espresso/AppTest.java
@@ -0,0 +1,16 @@
+package edu.jhu.espresso;
+
+//import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+//import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+public class AppTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
