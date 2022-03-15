3026c8bd8df9b97a64306dfed67cfa2784df77f8
Jared Allen <jla4782@truman.edu>
2022-03-15 19:43:17 -0400

    adding server project

---
+++ clueless-server/src/main/java/edu/jhu/espresso/App.java
@@ -0,0 +1,13 @@
+package edu.jhu.espresso;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
