793405e1068a5c60425b91f19135a484005d36fb
VanessaRupertus <100003877+VanessaRupertus@users.noreply.github.com>
2022-04-24 15:33:33 -0400

    added skeletal controller class & made the fxml a resource folder

---
+++ clueless-client/src/main/java/edu/jhu/espresso/client/domain/Controller.java
@@ -0,0 +1,42 @@
+package edu.jhu.espresso.client.domain;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Controller {
+
+    @FXML
+    private Button exit;
+
+    @FXML
+    private Button suggest;
+
+    @FXML
+    private Button accuse;
+
+    @FXML
+    private Button move;
+
+    @FXML
+    void accuseWindow(ActionEvent event) {
+
+    }
+
+    @FXML
+    void exitProgram(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sendMovement(ActionEvent event) {
+
+    }
+
+    @FXML
+    void suggestionWindow(ActionEvent event) {
+
+    }
+
+
+}
---
+++ clueless-client/src/main/java/edu/jhu/espresso/client/domain/Main.java
@@ -0,0 +1,21 @@
+package edu.jhu.espresso.client.domain;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("gameboardTest.fxml"));
+        primaryStage.setTitle("Clue-Less");
+        primaryStage.setScene(new Scene(root, 785, 594));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
